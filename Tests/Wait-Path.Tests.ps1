cdd9b959e2160d623c19b6a5135289cc54bee8a6
G8t Guy <g8tguy@g8tguy.com>
2017-01-15 02:33:41 +0500

    Bugz

--- Tests/Wait-Path.Tests.ps1
+++ Tests/Wait-Path.Tests.ps1
@@ -1,33 +1,36 @@
-
+Import-Module -Force $PSScriptRoot\..\Wait-Path.ps1
 
 Describe 'Wait-Path' {
     
-    Context 'Strict mode' {
-        
-        Set-StrictMode -Version latest
-        
-        It 'Should succeed on known paths' {
-            
+    Context 'Strict mode' { 
 
-            $Output = $null
+        Set-StrictMode -Version latest
+
+        It 'Should succeed on known paths' {
+            $StartTime = Get-Date
+            $Output = Wait-Path -Path C:\, HKLM:\System, ENV:Path, Function:Wait-Path
+            $EndTime = Get-Date
+
+            #No output expected, default timeout is 5 seconds
             $Output | Should BeNullOrEmpty
-            $true | Should be $True
+            ($EndTime - $StartTime).TotalSeconds -le 5 | Should be $True
         }
         
         It 'Should error on timeout without passthru' {
-            { throw "hello" } | Should Throw
+            { Wait-Path -Timeout 1 -Path C:\WhoOnEarthWouldHaveThisPath } | Should Throw
         }
-        
+
         It 'Should not error on timeout with passthru' {
-            {  } | Should Not Throw
+            { Wait-Path -Timeout 1 -Path C:\WhoOnEarthWouldHaveThisPath -Passthru } | Should Not Throw
         }
-        
+
         It 'Should return true when paths exist and passthru is used' {
-            $true | Should be $True
+            Wait-Path -Path C:\, HKLM:\System -Passthru | Should be $True
         }
         
         It "Should return false when paths don't exist or we time out and passthru is used" {
-            $false | Should be $False
+            Wait-Path -Timeout 1 -Path C:\WhoOnEarthWouldHaveThisPath -Passthru | Should be $False
         }
     }
 }
+
